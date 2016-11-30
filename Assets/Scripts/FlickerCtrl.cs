4d0be5c9a252e00a6985b55037261ed2348358c2
Kevin Whittaker <kevinlwhittaker@gmail.com>
2016-11-30 19:39:41 +0100

    adding particle system to cylinderbocken prefab

---
+++ Assets/Scripts/FlickerCtrl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickerCtrl : MonoBehaviour {
+	private ParticleSystem flame;
+	private Light light;
+	// Use this for initialization
+	void Start () {
+		flame = gameObject.GetComponent<ParticleSystem> ();
+		light = gameObject.GetComponentInChildren<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		light.enabled = (flame.particleCount%2 == 0);
+		Debug.Log (flame.particleCount%2 == 0);
+	}
+}
