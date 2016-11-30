2dc56cbf016a80df30ea0cac392599e076b0b7d9
Calle <cstenson@kth.se>
2016-11-30 17:19:46 +0100

    Gets corners from the parent to the border. However some round-offs makes it a bit off:/

--- Assets/Scripts/TrailMovement.cs
+++ Assets/Scripts/TrailMovement.cs
@@ -3,20 +3,43 @@ using System.Collections;
 
 public class TrailMovement : MonoBehaviour {
 
-    public Transform[] targets;
+    public Vector3[] targets;
     public Transform trail;
-    public float speed = 2.0f;
+    public float speed = 0.5f;
     public int laps = 2;
+
+    public float padding = 0.1f;
+
+    //Should be changed when image tracking starts on the target
+    public bool activated = false;
 
     private int index = 0;
     private int currentLap = 1;
 
+    void Start()
+    {
+
+        Vector3 parentScale = transform.parent.transform.localScale;
+        //Debug.Log(parentScale);
+        TrailRenderer TR = gameObject.GetComponent<TrailRenderer>();
+
+        float xScale = parentScale.x + padding;
+        float zScale = parentScale.z + padding;
+
+        //Find corners uses TransformPoint that goes from local to world coordinates
+        targets[0] = transform.TransformPoint(new Vector3(-xScale, 0.1f, zScale));
+        targets[1] = transform.TransformPoint(new Vector3(xScale, 0.1f, zScale));
+        targets[3] = transform.TransformPoint(new Vector3(-xScale, 0.1f, -zScale));
+        targets[2] = transform.TransformPoint(new Vector3(xScale, 0.1f, -zScale));
+        
+        //Debug.Log(targets[0] +" : "+ targets[1] + " : " + targets[2] + " : " + targets[3]);
+    }
     void FixedUpdate()
     {
         float step = speed * Time.fixedDeltaTime;
-        trail.position = Vector3.MoveTowards(trail.position, targets[index].position, step);
+        trail.position = Vector3.MoveTowards(trail.position, targets[index], step);
         
-        if(trail.position == targets[index].position)
+        if(activated && trail.position == targets[index])
         {
             if(index < targets.Length -1)
             {
@@ -32,6 +55,11 @@ public class TrailMovement : MonoBehaviour {
                 else
                 {
                     Destroy(this.gameObject);
+
+                    //to restore the border
+                    /*activated = false;
+                    index = 0;
+                    currentLap = 1;*/
                 }
             }
         }
