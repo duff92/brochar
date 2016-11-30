77155a233e1a268b55a11414159c977294b813d1
duff92 <joakimlarsson92@gmail.com>
2016-11-30 13:02:56 +0100

    Prefab for trail border

---
+++ Assets/Scripts/TrailMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailMovement : MonoBehaviour {
+
+    public Transform[] targets;
+    public Transform trail;
+    public float speed = 2.0f;
+    public int laps = 2;
+
+    private int index = 0;
+    private int currentLap = 1;
+
+    void FixedUpdate()
+    {
+        float step = speed * Time.fixedDeltaTime;
+        trail.position = Vector3.MoveTowards(trail.position, targets[index].position, step);
+        
+        if(trail.position == targets[index].position)
+        {
+            if(index < targets.Length -1)
+            {
+                index++;
+            }
+            else
+            {
+                if(currentLap < laps)
+                {
+                    currentLap++;
+                    index = 0;
+                }
+                else
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+
+    }
+}
