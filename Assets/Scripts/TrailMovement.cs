cbdd0716a28f82591b9d85c18cf9323ac51f3b3a
duff92 <joakimlarsson92@gmail.com>
2016-12-01 15:50:57 +0100

    Added a new version of border by using particle system instead of trail renderer

--- Assets/Scripts/TrailMovement.cs
+++ Assets/Scripts/TrailMovement.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class TrailMovement : MonoBehaviour {
 
-    public Vector3[] targets;
     public Transform trail;
     public float speed = 0.5f;
     public int laps = 2;
@@ -13,6 +12,7 @@ public class TrailMovement : MonoBehaviour {
     //Should be changed when image tracking starts on the target
     public bool activated = false;
 
+    private Vector3[] targets = new Vector3[4];
     private int index = 0;
     private int currentLap = 1;
 
@@ -21,18 +21,18 @@ public class TrailMovement : MonoBehaviour {
 
         Vector3 parentScale = transform.parent.transform.localScale;
         //Debug.Log(parentScale);
-        TrailRenderer TR = gameObject.GetComponent<TrailRenderer>();
 
-        float xScale = parentScale.x + padding;
-        float zScale = parentScale.z + padding;
+        float xScale = parentScale.x;
+        float zScale = parentScale.z;
 
+       // Debug.Log(xScale + ", " + zScale);
         //Find corners uses TransformPoint that goes from local to world coordinates
-        targets[0] = transform.TransformPoint(new Vector3(-xScale, 0.1f, zScale));
-        targets[1] = transform.TransformPoint(new Vector3(xScale, 0.1f, zScale));
-        targets[3] = transform.TransformPoint(new Vector3(-xScale, 0.1f, -zScale));
-        targets[2] = transform.TransformPoint(new Vector3(xScale, 0.1f, -zScale));
+        targets[0] = transform.TransformPoint(new Vector3(xScale, 0.1f, 0.0f));
+        targets[1] = transform.TransformPoint(new Vector3(xScale, 0.1f, -zScale));
+        targets[2] = transform.TransformPoint(new Vector3(0.0f, 0.1f, -zScale));
+        targets[3] = transform.TransformPoint(new Vector3(0.0f, 0.1f, 0.0f));
         
-        //Debug.Log(targets[0] +" : "+ targets[1] + " : " + targets[2] + " : " + targets[3]);
+        Debug.Log(targets[0] +" : "+ targets[1] + " : " + targets[2] + " : " + targets[3]);
     }
     void FixedUpdate()
     {
