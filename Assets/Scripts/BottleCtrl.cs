0a500b4237202e572d4b092fd15128cde1063d85
Kevin Whittaker <kevinlwhittaker@gmail.com>
2016-12-05 15:54:44 +0100

    adding throwing behavior

---
+++ Assets/Scripts/BottleCtrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleCtrl : MonoBehaviour {
+
+	void Update(){
+		
+	}
+
+	void OnCollisionEnter(Collision collision) {
+
+	}
+
+	public void Reset(){
+		transform.localPosition = new Vector3 (-0.15f, -0.13f, 0.48f);
+		gameObject.GetComponent<Rigidbody> ().isKinematic = true;
+	}
+
+	public void Throw(Vector3 target){
+		gameObject.GetComponent<Rigidbody> ().isKinematic = false;
+		Vector3 direction = target - transform.position;
+		float force = direction.magnitude;
+		direction = direction.normalized;
+		direction.z = 0.5f;
+		gameObject.GetComponent<Rigidbody> ().AddForce (direction * force * 100);
+		// test
+	}
+}
---
+++ Assets/Scripts/GoatCtrl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoatCtrl : MonoBehaviour {
+	public BottleCtrl molotov;
+	private bool thrown = false;
+	private Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.localPosition;
+
+	}
+
+	void OnCollisionEnter(){
+		ParticleSystem[] flame = gameObject.GetComponentsInChildren<ParticleSystem> ();
+		for (int i = 0; i < flame.Length; i++) {
+			flame [i].Play ();
+		}
+		AudioSource[] audio = gameObject.GetComponentsInChildren<AudioSource> ();
+		for (int i = 0; i < audio.Length; i++) {
+			audio [i].Play ();
+		}
+		Destroy (molotov);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.T) && !thrown) {
+				thrown = true;
+				molotov.Throw (transform.position);
+		}else if(Input.GetKey(KeyCode.R) && thrown){
+			thrown = false;
+			molotov.GetComponent<BottleCtrl> ().Reset ();
+		}
+	}
+}
